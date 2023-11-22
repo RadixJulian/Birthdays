9f97729cbf2ae5bbcdc1176c7e9ebd951de049ca
RadixJulian <150384915+RadixJulian@users.noreply.github.com>
2023-11-22 10:50:59 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -25,10 +25,10 @@ export default function Header() {
         <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-indigo-500 animated-gradient">
           <div className="w-full max-w-md p-4 md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-4xl">
             <div className="bg-white rounded-xl p-8 shadow-lg">
-              <h1 className="uppercase text-2xl md:text-3xl lg:text-4xl text-center text-white bg-primary p-2 rounded-t-md mb-4">
+              <h1 className="uppercase text-4xl md:text-3xl lg:text-4xl text-center text-white bg-primary p-2 rounded-t-md mb-4">
                 🎂 Birthday Info 🎂
               </h1>
-              <div className="mb-6 text-lg md:text-xl text-center text-black font-semibold">
+              <div className="mb-6 text-4xl md:text-xl text-center text-black font-semibold">
                 <p>
                   Current Date: {m}/{d}/{y}
                 </p>
