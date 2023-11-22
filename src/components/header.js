5a7102de29619164d05a2da19dd020be9c2d8f77
RadixJulian <150384915+RadixJulian@users.noreply.github.com>
2023-11-22 10:48:09 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -26,7 +26,7 @@ export default function Header() {
           <div className="w-full max-w-md p-4 md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-4xl">
             <div className="bg-white rounded-xl p-8 shadow-lg">
               <h1 className="uppercase text-2xl md:text-3xl lg:text-4xl text-center text-white bg-primary p-2 rounded-t-md mb-4">
-                Birthday Info
+                🎂 Birthday Info 🎂
               </h1>
               <div className="mb-6 text-lg md:text-xl text-center text-black font-semibold">
                 <p>
